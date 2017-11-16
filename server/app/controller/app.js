a3c378b51287d31c139fd11dce0ee62a39c0206a
penfan <fangpei9212@163.com>
2017-11-16 22:09:19 +0800

    爬取“发现页”问题的标题、地址、浏览数、回答数、关注数

--- server/app/controller/app.js
+++ server/app/controller/app.js
@@ -1,16 +1,40 @@
-var request = require('request-promise-native')
+let request = require('request-promise-native')
+let cheerio = require('cheerio')
+const ZHIHU = 'https://www.zhihu.com'
+const getQuestions = async (ques) => {
+  const promises = ques.map( item => {
+    return request(`${ZHIHU}${item.url}`).then(res => {
+      const $ = cheerio.load(res)
+      const NumberBoad = $('.NumberBoard-item .NumberBoard-value')
+      item.followers = $(NumberBoad[0]).text()
+      item.readers = $(NumberBoad[1]).text()
+      item.answers = $('h4.List-headerText span').text().replace(' 个回答','')
+    })
+  })
+  await Promise.all(promises)
+}
+
 module.exports = {
   index(ctx){
     ctx.body = '首页'
   },
   async spider(ctx) {
-    const rs = await request('http://www.baidu.com').catch(err => {
+    const res = await request(`${ZHIHU}/explore`).catch(err => {
       return err
     })
-    if (rs.error){
-      ctx.body = rs.message
-    }else{
-      ctx.body = rs
+    if (res.error){
+      ctx.body = res.message
+      return
     }
+    const $ = cheerio.load(res)
+    let questions = [];
+    $('.explore-tab .tab-panel h2 a').each((index,item) => {
+      questions.push({
+        title: $(item).text(),
+        url: $(item).attr('href').split('/answer')[0]
+      })
+    })
+    await getQuestions(questions)
+    ctx.body = questions
   }
 }
